6961a680ae9731437a03ac1a1bec36380c7e6965
SadiniWanniarachchi <sadiiw17@gmail.com>
2025-02-18 13:55:57 +0530

    feat-order management done

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -308,10 +308,10 @@ const Orders = () => {
                                         <td className="py-3 px-6">
                                             <span
                                                 className={`px-3 py-1 rounded-full text-sm ${order.status === "Pending"
-                                                        ? "bg-yellow-100 text-yellow-800"
-                                                        : order.status === "Delivered"
-                                                            ? "bg-green-100 text-green-800"
-                                                            : "bg-red-100 text-red-800"
+                                                    ? "bg-yellow-100 text-yellow-800"
+                                                    : order.status === "Delivered"
+                                                        ? "bg-green-100 text-green-800"
+                                                        : "bg-red-100 text-red-800"
                                                     }`}
                                             >
                                                 {order.status}
